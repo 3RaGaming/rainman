23aa0405d7b9c899bb52383e92833ef276fce790
Carl <carlalanfunk@outlook.com>
2017-01-03 18:48:23 -0600

    Server Changes
    Fix server code to be used for players (x, y);

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 
 
 
-var ships = [];
+var players = [];
 
-function Ship() {
-	//this.id = id;
-	this.pos = pos;
-	this.r = r;
-	this.heading = heading;
-	
+function Player(id, xPos, yPos) {
+	this.id = id;
+	this.x = xPos;
+  this.y = yPos;
 }
 
 var express = require('express');
@@ -24,45 +22,39 @@ var socket = require('socket.io');
 
 var io = socket(server);
 
-io.sockets.on('connection',
-
-function(socket) {
-	
+io.sockets.on('connection', function(socket) {
 	console.log('We have a new client: ' + socket.id);
-	
 
-	socket.on('start',
-		function(data) {
-			
-			
+	socket.on('start', function(data) {
 		//console.log(socket.id + " " + data.x + " " + data.y + " " + data.r + " " +data.heading);
-		var ship = new Ship(socket.id, data.pos, data.r, data.heading);
-		ships.push(ship);
+		var p = new Player(socket.id, data.x, data.y);
+		players.push(p);
 		//socket.broadcast.emit('mouse', data);
-	}
-   );
+	});
 
-   socket.on('update',
-      function(data) {
+    socket.on('update', function(data) {
+        console.log(data); 
         //console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
-        var ship;
-        for (var i = 0; i < ships.length; i++) {
-          if (socket.id == ships[i].id) {
-            ship = ships[i];
+        var p;
+        for (var i = 0; i < players.length; i++) {
+          if (socket.id == players[i].id) {
+            p = players[i];
           }
         }
-        ship.pos = data.pos;
-        ship.r = data.r;
-		ship.heading = data.heading;
-      }
-    );
 
-	socket.on('disconnect', function() {
-      console.log("Client has disconnected");
-    });
+        if (p){
+          p.x = data.x;
+          p.y = data.y;
+        }
 
-   }
-);
+
+  });
+
+  socket.on('disconnect', function() {
+    console.log("Client has disconnected");
+  });
+
+});
 
 
 
